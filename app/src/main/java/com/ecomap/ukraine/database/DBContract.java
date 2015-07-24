2d3c4a61b57b61128a90c585b7c92e80b121fc64
Oleh Bernov <oleh.bernov@gmail.com>
2015-07-24 16:26:33 +0300

    SQLiteOpenHelper added

--- app/src/main/java/com/ecomap/ukraine/database/DBContract.java
+++ app/src/main/java/com/ecomap/ukraine/database/DBContract.java
@@ -6,25 +6,65 @@ import android.provider.BaseColumns;
  * Created by Oleh on 7/24/2015.
  */
 public class DBContract {
-    public static final String CREATE_PROBLEMS_TABLE = "";
+    public static final String CREATE_PROBLEMS_TABLE =
+            "CREATE TABLE ";
     public static final String CREATE_DETAILS_TABLE = "";
     public static final String CREATE_PHOTO_TABLE = "";
     public static final String CREATE_ACTIVITY_TABLE = "";
     public static final String DELETE_ALL_TABLES = "";
 
     public static abstract class Problems implements BaseColumns {
+        public static final String TABLE_NAME = "Problems";
 
+        public static final String ID = "Id";
+
+        public static final String PROBLEM_STATUS = "Status";
+
+        public static final String PROBLEM_TYPES_ID = "ProblemTypes_Id";
+
+        public static final String PROBLEM_DATE = "Date";
+
+        public static final String LATITUDE = "Latitude";
+
+        public static final String LONGITUDE = "Longitude";
     }
+
     public static abstract class Details implements BaseColumns {
+        public static final String TABLE_NAME = "Details";
+
+        public static final String PROBLEM_ID = "Problem_ID";
+
+        public static final String PROBLEM_CONTENT = "Content";
+
+        public static final String PROPOSAL = "Proposal";
+
+        public static final String MODERATION = "Moderation";
+
+        public static final String SEVERITY = "Severity";
+
+        public static final String VOTES = "Votes";
+    }
+
+    public static abstract class Photos implements BaseColumns {
+        public static final String TABLE_NAME = "Photos";
+
 
     }
 
-    public static abstract class Photo implements BaseColumns {
+    public static abstract class ProblemActivity implements BaseColumns {
+        public static final String TABLE_NAME = "Activities";
 
-    }
+        public static final String PROBLEM_ID = "Problem_ID";
 
-    public static abstract class ProblemActivity implements BaseColumns{
+        public static final String ACTIVITY_TYPES_ID = "ActivityTypes_Id";
 
+        public static final String COMMENT_USERS_ID = "Users_Id";
+
+        public static final String PROBLEM_ACTIVITY_DATE = "Date";
+
+        public static final String PROBLEM_ACTIVITY_CONTENT = "Content";
+
+        public static final String USER_NAME = "userName";
     }
 
 }
