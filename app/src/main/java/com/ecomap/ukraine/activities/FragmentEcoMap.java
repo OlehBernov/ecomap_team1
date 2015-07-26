557a5fe2e34e30ee998cbea2b6e9bedc509caf98
amukysh <andriy.mukysh@gmail.com>
2015-07-26 22:55:56 +0300

    Add clustering

--- app/src/main/java/com/ecomap/ukraine/activities/FragmentEcoMap.java
+++ app/src/main/java/com/ecomap/ukraine/activities/FragmentEcoMap.java
@@ -1,42 +1,41 @@
 package com.ecomap.ukraine.activities;
 
-import android.app.Fragment;
+import android.graphics.Point;
 import android.os.Bundle;
-import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.androidmapsextensions.ClusterGroup;
+import com.androidmapsextensions.ClusteringSettings;
+import com.androidmapsextensions.GoogleMap;
+import com.androidmapsextensions.Marker;
+import com.androidmapsextensions.MarkerOptions;
 import com.ecomap.ukraine.R;
-import com.ecomap.ukraine.data.manager.DataListener;
 import com.ecomap.ukraine.data.manager.DataManager;
 import com.ecomap.ukraine.data.manager.ProblemListener;
-import com.ecomap.ukraine.database.DBHelper;
 import com.ecomap.ukraine.models.Problem;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapView;
-import com.google.android.gms.maps.MapsInitializer;
-import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.Projection;
 import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.List;
+
+
 
 /**
  * Created by Andriy on 25.07.2015.
  */
-public class FragmentEcoMap extends android.support.v4.app.Fragment implements ProblemListener {
-    private MapView mMapView;
-    private GoogleMap googleMap;
+public class FragmentEcoMap extends BaseFragment implements ProblemListener {
     private DataManager manager = DataManager.getInstance();
     private static final  LatLng INTIAL_POSITION = new LatLng(48.4 , 31.2);
+    private List<Marker> declusterifiedMarkers;
 
-    private static int MOVING_TIME = 2000;
 
     public static FragmentEcoMap newInstance() {
         FragmentEcoMap fragment = new FragmentEcoMap();
@@ -44,6 +43,7 @@ public class FragmentEcoMap extends android.support.v4.app.Fragment implements P
     }
 
     public FragmentEcoMap() {
+        super();
     }
 
     @Override
@@ -51,54 +51,37 @@ public class FragmentEcoMap extends android.support.v4.app.Fragment implements P
                              Bundle savedInstanceState) {
         manager.registerProblemListener(this);
 
-        View rootView = inflater.inflate(R.layout.fragement_map, container, false);
-        mMapView = (MapView) rootView.findViewById(R.id.mapView);
-        mMapView.onCreate(savedInstanceState);
-
-        mMapView.onResume();// needed to get the map to display immediately
-
-        try {
-            MapsInitializer.initialize(getActivity().getApplicationContext());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        this.setUpMapIfNeeded();
-
-        return rootView;
+        return inflater.inflate(R.layout.simple_map, container, false);
     }
 
-    private void setUpMapIfNeeded() {
-        // Do a null check to confirm that we have not already instantiated the map.
-        if (googleMap == null) {
-            // Try to obtain the map from the SupportMapFragment.
-            googleMap = mMapView.getMap();
-            // Check if we were successful in obtaining the map.
-            if (googleMap != null) {
-                setUpMap();
-            }
-        }
-    }
-    private void setUpMap() {
+
+    @Override
+    protected void setUpMap() {
+
         CameraPosition cameraPosition = new CameraPosition.Builder()
                 .target(new LatLng(INTIAL_POSITION.latitude, INTIAL_POSITION.longitude)).zoom(5).build();
-        googleMap.animateCamera(CameraUpdateFactory
-                .newCameraPosition(cameraPosition), MOVING_TIME, null);
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newCameraPosition(cameraPosition);
+        map.moveCamera(cameraUpdate);
 
-        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-        UiSettings settings = googleMap.getUiSettings();
-        googleMap.setMyLocationEnabled(true);
+        UiSettings settings = map.getUiSettings();
+        map.setMyLocationEnabled(true);
         settings.setMyLocationButtonEnabled(true);
         manager.getAllProblems();
+        map.setClustering(new ClusteringSettings().clusterOptionsProvider(new DemoClusterOptionsProvider(getResources())));
+
+
     }
 
     public void putAllProblemsOnMap(List<Problem> problems) {
+        MarkerOptions options = new MarkerOptions();
         for (Problem problem : problems) {
             MarkerOptions marker = new MarkerOptions().position(
                     new LatLng(problem.getPosition().latitude, problem.getPosition().longitude)).title(problem.getTitle());
             BitmapDescriptor image = BitmapDescriptorFactory.fromResource(this.getResourceIdForMarker(
                     problem.getProblemTypesId()));
             marker.icon(image);
-            googleMap.addMarker(marker);
+            map.addMarker(marker);
+
         }
 
     }
@@ -135,4 +118,9 @@ public class FragmentEcoMap extends android.support.v4.app.Fragment implements P
     public void update(int requestType, Object problem) {
         putAllProblemsOnMap((List<Problem>) problem);
     }
+
+
+
+
+
 }
