426e12018d41b285cf8a827f0e1b158e62d069f0
Oleksandr Kyrychenko <kyrychenko.olexandr@gmail.com>
2015-08-01 13:56:12 +0300

    library

--- app/src/main/java/com/ecomap/ukraine/activities/MainActivity.java
+++ app/src/main/java/com/ecomap/ukraine/activities/MainActivity.java
@@ -15,7 +15,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 import com.codetroopers.betterpickers.calendardatepicker.CalendarDatePickerDialog;
 import com.ecomap.ukraine.R;
